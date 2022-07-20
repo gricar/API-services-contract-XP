c7e2691e6d2099f588705cfe52e5db31ff6c7e4e
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-07-20 10:37:04 -0300

    refactor: arrow function standard

--- src/banking/service/index.ts
+++ src/banking/service/index.ts
@@ -6,7 +6,7 @@ import { IClientBalance } from '../interfaces/IClientBalance';
 export default class BankingService {
   private model = BalanceClientsBroker;
 
-  public async getAll(): Promise<IClientBalance[]> {
+  public getAll = async (): Promise<IClientBalance[]> => {
     const allClients = await this.model.findAll({
       attributes: ['clientCode', 'balance'],
       include: [
@@ -16,7 +16,7 @@ export default class BankingService {
     });
 
     return allClients;
-  }
+  };
 
   public getOne = async (clientCode: number) => {
     const client = await this.model.findAll({
