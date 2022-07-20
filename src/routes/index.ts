7bea2b4ec48384fce75c587faf9d3af118919a4e
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-07-20 10:01:02 -0300

    feat: route nonexistent

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,5 +6,6 @@ const routes = Router();
 routes.use('/banking/conta', bankingRoute);
 
 routes.get('/', (_req, res) => res.send('Hello world'));
+routes.all('*', (req, _res, next) => next({ message: `Route '${req.path}' doesn't exist!` }));
 
 export default routes;
