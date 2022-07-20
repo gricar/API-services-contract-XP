137b8f7b4f27f93f5e1d433fbae62edebca6a359
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-07-20 14:34:21 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import 'express-async-errors';
 import express from 'express';
+import cors from 'cors';
 import errorHandler from './middlewares/errorHandler';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use(routes);
+
 app.use(errorHandler);
 
 export default app;
